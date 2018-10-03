33016d61de86147b5179103edfb4dfdd2a8c51a9
Bhavesh Shrimali <bhavesh.shrimali@gmail.com>
2018-10-03 07:44:56 -0500

    Add files via upload

--- ProblemFEM.py
+++ ProblemFEM.py
@@ -6,10 +6,12 @@ Created on Thu Sep 27 21:37:11 2018
 
 Sparse Matrix as such not needed for the small dimensionality of the problem!
 Solving a 1D BVP:
+    
     -(EA(x)u'(x))'+Cu=T  for x in (0,L)
     u(0)=u(1)=0  : Homogeneous BC's
 
-with quadratic Lagrange elements 
+with quadratic Lagrange elements , 
+now (hopefully) with quadratic (at least) p-Hierarchical elements
 Parameters: 
     
     L  = Length of the domain
@@ -22,6 +24,8 @@ FIX ME:
     change the exact solution for plotting 
     include p-hierarchical shape functions (FEM not GFEM)
     Lines 178, 179 in implementation of higher order FE (how to properly dimension globK, globF etc. without touching nodes )
+    Lines 191, 192.. in relating the global dof # to the element # to get global system
+
 """
 
 import numpy as np
@@ -40,11 +44,11 @@ class geometry() : #1D geometry parameters
         self.A=1.
         self.E=1.
         self.C=0.
-        self.a=500.
+        self.a=0.5
         self.xb=0.2
         self.nLNodes=ne+1       #Linear elements
         self.nQNodes=2*ne+1     #Quadratic elements 
-        self.nNNodes=ne*deg+1   #Isoparametric deg-th order lagrange elements 
+        self.nNNodes=int(ne*deg+1)   #Isoparametric deg-th order lagrange elements 
         self.ndim=1
         self.M=0
         self.g=0.
@@ -58,12 +62,12 @@ def T(x,a,xb):
 
 class GPXi():
     def __init__(self,ordr):
-        from numpy.polynomial.legendre import leggauss   #Gauss-Legendre Quadrature for 1D
-#        from numpy.polynomial.chebyshev import chebgauss #Gauss-Chebyshev Quadrature for 1D (bad!)
+        from numpy.polynomial.legendre import leggauss                         #Gauss-Legendre Quadrature for 1D
+#        from numpy.polynomial.chebyshev import chebgauss                      #Gauss-Chebyshev Quadrature for 1D (bad!)
         self.xi=leggauss(ordr)[0]
         self.wght=leggauss(ordr)[1]
 
-class basis():  # defined on the canonical element (1D : [-1,1] ) 
+class basis():                                                                 # defined on the canonical element (1D : [-1,1] ) 
     def __init__(self,deg,basis_type):
         deg = int(deg)
         from sympy import Symbol,diff,Array,lambdify,legendre,integrate,simplify,permutedims
@@ -90,7 +94,7 @@ class basis():  # defined on the canonical element (1D : [-1,1] )
 #                dfN=diff(N,z)
 #                self.Ns=lambdify(z,N,'numpy')
 #                self.dN=lambdify(z,dfN,'numpy')
-#            elif deg==4.:                                                      #not implemented yet. the no. of nodes change. define a general geom.nNnodes (?)
+#            elif deg==4.:                                                     #not implemented yet. the no. of nodes change. define a general geom.nNnodes (?)
 #                N=1/2*Array([z*(z-1),2*(1+z)*(1-z),z*(1+z)])
 #                dfN=diff(N,z)
 #                self.Ns=lambdify(z,N,'numpy')
@@ -136,48 +140,39 @@ MapX='L2'                                                                      #
 nB=basis(float(MapX[1]),MapX[0])
 B=basis(float(El[1]),El[0])                                                    #Basis for FE fields (isoparametric) 
 Np = 30                                                                        #Order of Gauss-Integration for the Discrete Galerkin-Form 
-Nel=500
+Nel=2
 geom=geometry(Nel,float(El[1]))
 GP=GPXi(Np)
 
 def loc_mat(nodes,a,xb):                                                       #Computes the element quantities when supplied with gloabl nodes
     xi=GP.xi;W=GP.wght
-#    if El[1]=='2': # fix this if condition 
-#        x=np.array(B.Ns(xi)).T @ nodes    # Mapping for linear 1D
-#        Je=(np.array(B.dN(xi)).T @ nodes)*np.ones(xi.size,)        #Jacobian for 1D mapping
-#    else:
     x=np.array(nB.Ns(xi)).T @ nodes
     Je=np.array(nB.dN(xi)).T @ nodes
 #    print(Je)
     if El[0]=='L' or El[0]=='M':                                               # 1D lagrange or legendre elements
         b1 = np.array(B.Ns(xi)).reshape(-1,1,W.size)
-#        if El=='L2':
-#            a1 = np.array(B.dN(xi)).repeat(W.size).reshape(2,-1).reshape(-1,1,W.size)
-#        else: 
         a1=np.array(B.dN(xi)).reshape(-1,1,W.size)  
         a2=a1.reshape(1,len(a1),-1).copy()
         b2=b1.reshape(1,len(b1),-1).copy()
         a1 *= geom.E/Je*geom.A*W                                               #multiply by weights, jacobian, A, bitwise 
         b1 *= geom.C*Je*W
         mat=np.tensordot(a1,a2,axes=([1,2],[0,2])) + np.tensordot(b1,b2,axes=([1,2],[0,2]))
-#        elemF=np.dstack((np.array(B.Ns(xi[i]))*W[i]*Je[i]*b(x[i]) for i in range(xi.size)))
-#        elemF=elemF.sum(axis=2).flatten()
         elemF=(np.array(B.Ns(xi))*W*Je*T(x,a,xb)).sum(axis=1).flatten()
         return mat,elemF
 
 
-if float(El[1])==1.:
-    nodes=np.linspace(0.,geom.L,geom.nLNodes) # Global nodal coordinates 
+if float(MapX[1])==1.:                                                         # This is the physical coordinates and hence dependent on MapX not El
+    nodes=np.linspace(0.,geom.L,geom.nLNodes)                                  # Global nodal coordinates 
     elems=np.vstack( (np.arange(1,geom.nLNodes,1),np.arange(2,geom.nLNodes+1,1)) ).astype(int).T-1 # only for linear 1D lagrange 
-elif float(El[1])==2.:
-    nodes=np.linspace(0.,geom.L,geom.nQNodes) # Global nodal coordinates
+elif float(MapX[1])==2.:
+    nodes=np.linspace(0.,geom.L,geom.nQNodes)                                  # Global nodal coordinates
     elems=np.vstack( (np.arange(1,geom.nQNodes,2),
                       np.arange(2,geom.nQNodes+1,2),
                       np.arange(3,geom.nQNodes+2,2))).astype(int).T-1          # only for quadr. 1D lagrange
 
 
-globK=0*np.eye(len(nodes)*geom.ndim)
-globF=np.zeros(len(nodes)*geom.ndim)
+globK=0*np.eye(geom.nNNodes)                                                   # This changes because now we no longer have iso-p map
+globF=np.zeros(geom.nNNodes)                                                   # This changes too, again because no iso-p map 
 dof=np.inf*np.ones(len(globK))                                                 #initialize 
 prescribed_dof=np.array([[0,0],
                          [-1,0]])
@@ -185,12 +180,17 @@ prescribed_forc=0*np.array([[-1,-geom.M*geom.g]])                              #
 
 dof[prescribed_dof[:,0]]=prescribed_dof[:,1]
 
-for k in range(elems[:,0].size):  #assembly
+for k in range(elems[:,0].size):                                               #Assembly of Global System
     elnodes=elems[k]
-    if El[1]=='1':
-        globdof=np.array([geom.ndim*elnodes[0],geom.ndim*elnodes[1]])
-    elif El[1]=='2':
-        globdof=np.array([geom.ndim*elnodes[0],geom.ndim*elnodes[1],geom.ndim*elnodes[2]])
+    if El[0]=='L':
+        if El[1]=='1':
+            globdof=np.array([geom.ndim*elnodes[0],geom.ndim*elnodes[1]])
+        elif El[1]=='2':
+            globdof=np.array([geom.ndim*elnodes[0],geom.ndim*elnodes[1],geom.ndim*elnodes[2]])
+    elif El[0]=='M':
+        elemord=int(El[1])+1
+        globdof = np.array([k*(elemord-1)+i for i in range(elemord)],int)
+        print(globdof)
     if El[0]=='L' or El[0]=='M':                                               # 1D lagrange or legendre polynomials 
         nodexy=nodes[elnodes]
     else:
@@ -252,14 +252,18 @@ def plotfigs(xp):
     ax.set_title(r'Stress',fontsize=14)
 #    ax.yaxis.set_minor_locator(my)    
 
-def fsample(x,dof,nodes):                                                            #giving out the displacement, strain and stress at x: GP and dof value of disp. at gauss point
+def fsample(x,dof,nodes):                                                      #giving out the displacement, strain and stress at x: GP and dof value of disp. at gauss point
     dspl=splrep(nodes,dof,k=int(El[1]))
     return splev(x,dspl)
 
 #plotfigs(xplot) 
 ue=uex(xplot,geom.a,geom.xb)
 xpl2 = np.linspace(nodes[0],nodes[-1],10*geom.nQNodes-1)
-uapp=fsample(xpl2,dof[0:len(dof):int(El[1])],nodes[0:len(nodes):2])
+if El[0]=='M':
+    uapp=fsample(xpl2,dof[0:len(dof):int(El[1])],nodes[0:len(nodes):2])
+elif El[0]=='L':
+    uapp=fsample(xpl2,dof,nodes)
+    
 y_a=np.zeros((2,nodes.size))
 y_b=np.zeros((2,nodes.size))
 
@@ -267,7 +271,9 @@ res_a=solve_bvp(fexact,fbc,nodes,y_a,p=[geom.a,geom.xb])
 
 plt.figure(figsize=(8,8))
 plt.plot(xplot,res_a.sol(xplot)[0],label=r'Exact Solution')
-plt.text(0.5,0.6,r'$a=$'+str(geom.a),fontsize=22)
+ax=plt.gca()
+plt.text(0.5,0.6,r'$a=$'+str(geom.a),transform=ax.transAxes,
+         fontsize=22,bbox=dict(facecolor='red', alpha=0.5))
 if El[0]=='L':
     plt.plot(xpl2,uapp,'rx',label=r'Quadratic Lagrange FE')
 elif El[0]=='M':
@@ -276,3 +282,6 @@ plt.tick_params(axis='both',which='both',direction='in',top=True,right=True,labe
 plt.title(r'Number of elements: '+str(Nel)+', NGP = '+str(Np),fontsize=20)
 plt.legend(loc=0,fontsize=18)
 plt.grid(True,linestyle='--')
+plt.text(0.2,0.2,r'$U^h$ = '+str(0.5*dof @ (globK @ dof)),transform=ax.transAxes,
+         fontsize=22,bbox=dict(facecolor='red', alpha=0.5))
+print(0.5*dof @ (globK @ dof))
